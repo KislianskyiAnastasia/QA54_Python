ef42d9508bb05ad029b38b865f8a7bbcdffd865e
AnastasiaKislianskyi <Kislianskyian@gmail.com>
2026-09-24 21:04:23 +0300

    lesson3

--- lesson3_1_functionPRO/kwargs.py
+++ lesson3_1_functionPRO/kwargs.py
@@ -9,3 +9,12 @@ def create_user3(**data):
 
 user = create_user3(name="Anastasia",role="student")
 print(user)
+
+# user=create_user3("name": "Kris")
+def for_example(a,b=15,*args,**kwargs):
+    print(a)
+    print(b)
+    print(args)
+    print(kwargs)
+
+for_example(2,3,4,5,name="Anastasia")
